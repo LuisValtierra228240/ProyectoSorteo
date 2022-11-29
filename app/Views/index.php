33e5ae3382dc0de8d3d0120c65cd75bbf0d46634
Gerardo <lgerardoaramburo@gmail.com>
2022-11-28 20:31:55 -0700

    Agregue el nombre a pantalla principal

--- app/Views/index.php
+++ app/Views/index.php
@@ -7,7 +7,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Inicio</h1>
+                <h1 class="page-header">Hola, <?= $usuario["nombre"]?></h1>
             </div>
             <!-- /.col-lg-12 -->
         </div>
